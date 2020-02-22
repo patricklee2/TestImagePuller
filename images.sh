e1fc93b37f559ad26075288aaaeba3c50086eb4e
Ubuntu <patle@patle-linux-vm.nabjlwwesxuunhukc0qgxbxycc.xx.internal.cloudapp.net>
2020-02-22 01:33:14 +0000

    a

--- images.sh
+++ images.sh
@@ -3,10 +3,12 @@ mkdir "/var/LWASFiles/Sites/testimagepuller/home/site/wwwroot/images"
 file="/var/LWASFiles/Sites/testimagepuller/home/site/wwwroot/images/images.json"
 #file='/home/patle/TestImagePuller/images/images.json'
 
-content=`docker images --format "{{json . }}"`
+content=`docker images --format "{{json . }},"`
 if [[ -f "$file" ]]; then
     rm $file
 fi
 
 touch $file
-echo $content > $file
+echo "{\"images\": [" > $file
+echo $content >> $file
+echo  "{}]}" >> $file
--- init.sh
+++ init.sh
@@ -1,7 +1,11 @@
-# m h dom mon dow   command
+mkdir images
+mkdir remove
+mkdir pullandtag
+
 chmod u+x ~/TestImagePuller/images.sh
 chmod u+x ~/TestImagePuller/pull.sh
 chmod u+x ~/TestImagePuller/remove.sh
-echo "*/5 * * * * ~/TestImagePuller/images.sh" >> /var/spool/cron/crontabs/root
+# m h dom mon dow   command
+echo "* * * * * ~/TestImagePuller/images.sh" >> /var/spool/cron/crontabs/root
 echo "*/5 * * * * ~/TestImagePuller/pull.sh"   >> /var/spool/cron/crontabs/root
 echo "*/5 * * * * ~/TestImagePuller/remove.sh" >> /var/spool/cron/crontabs/root
