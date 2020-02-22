7de5a853ff887455560c62b159da4257fecb364d
Ubuntu <patle@patle-linux-vm.nabjlwwesxuunhukc0qgxbxycc.xx.internal.cloudapp.net>
2020-02-22 01:15:56 +0000

    a

--- images.sh
+++ images.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+mkdir "var/LWASFiles/Sites/testimagepuller/home/site/wwwroot/images"
 file="/var/LWASFiles/Sites/testimagepuller/home/site/wwwroot/images/images.json"
 #file='/home/patle/TestImagePuller/images/images.json'
 
